6278309f8ef0d4d124c0232e205b9f8a77c63b14
SenyorSchmidt <155657963+SenyorSchmidt@users.noreply.github.com>
2025-01-13 20:12:57 +0100

    Updated comments and todos. optimized navbar and techstack

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -6,3 +6,6 @@
  */
 
 // ** TODO: ADD GSAP Animation to component (bars sliding in from the sides, depending on scroll position)
+// Display Website as a tile with a picture, description and link. Like Meta card
+// Display GSAP on website with animations (Bars sliding in and out depending on position of screen)
+// Display 3.js animation as a spinning globe
